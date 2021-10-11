50fec454192888a1512fc2b0d84109b36d4f48df
Bhajanpreet Singh <bhajanpreet25@gmail.com>
2021-10-10 23:27:06 -0400

    formatting

--- BakersPoint.Infrastructure/BakersPointDbContext.cs
+++ BakersPoint.Infrastructure/BakersPointDbContext.cs
@@ -11,19 +11,18 @@ namespace BakersPoint.Infrastructure
     {
         DbSet<Product> Products { get; set; }
         DbSet<Invoice> Invoices { get; set; }
-
         Task SaveChangesAsync();
     }
 
     public class ApplicationContext : DbContext, IApplicationContext
     {
-        public ApplicationContext(DbContextOptions<ApplicationContext> options): base(options)
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
         {
         }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Invoice> Invoices { get; set; }
-
         public async Task SaveChangesAsync()
         {
             await base.SaveChangesAsync();
@@ -34,7 +33,7 @@ namespace BakersPoint.Infrastructure
     { 
         public ApplicationContext CreateDbContext(string[] args) 
         { 
-            IConfigurationRoot configuration = new ConfigurationBuilder()
+            var configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile(@Directory.GetCurrentDirectory() + "/../BakersPoint.Api/appsettings.json")
                 .Build(); 
