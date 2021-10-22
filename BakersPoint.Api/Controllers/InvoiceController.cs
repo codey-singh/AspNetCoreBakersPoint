43d1cb1c0e03d2f9220502caec783f6f425bd628
Bhajanpreet Singh <bhajanpreet25@gmail.com>
2021-10-21 21:51:28 -0400

    minor refactoring and logging corrections

--- BakersPoint.Api/Controllers/InvoiceController.cs
+++ BakersPoint.Api/Controllers/InvoiceController.cs
@@ -9,10 +9,10 @@ namespace BakersPoint.Api.Controllers
     [ApiController]
     public class InvoiceController : ControllerBase
     {
-        private readonly ILogger<ProductController> _logger;
+        private readonly ILogger<InvoiceController> _logger;
         private readonly IInvoiceService _invoiceService;
 
-        public InvoiceController(ILogger<ProductController> logger, IInvoiceService invoiceService)
+        public InvoiceController(ILogger<InvoiceController> logger, IInvoiceService invoiceService)
         {
             _logger = logger;
             _invoiceService = invoiceService;
@@ -26,7 +26,7 @@ namespace BakersPoint.Api.Controllers
             return Ok(invoices);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id:int}")]
         public async Task<IActionResult> GetById(int id)
         {
             var invoice = await _invoiceService.GetInvoiceById(id);
